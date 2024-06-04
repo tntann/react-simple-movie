a6796342907da7ab35e427e804bb2bed9c36a769
tntan <nhattan8421@gmail.com>
2024-06-04 22:34:25 +0700

    add config

---
+++ src/config.jsx
@@ -0,0 +1,3 @@
+export const fetcher = (...args) => fetch(...args).then((res) => res.json());
+export const apiKey = "247c6a0e12abe3d34fcf5e59c2032141";
+//https://api.themoviedb.org/3/movie/now_playing?&api_key=247c6a0e12abe3d34fcf5e59c2032141
