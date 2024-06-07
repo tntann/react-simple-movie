08919e0709b2b5fdce1a178883cd3188f056258d
tntan <nhattan8421@gmail.com>
2024-06-07 21:58:34 +0700

    update Button

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Button = ({
+  onClick,
+  className,
+  full = false,
+  type = "button",
+  bgColor = "primary",
+  children,
+}) => {
+  let bgClassName = "bg-primary";
+  switch (bgColor) {
+    case "primary":
+      bgClassName = "bg-primary";
+      break;
+    case "secondary":
+      bgClassName = "bg-secondary";
+      break;
+    default:
+      break;
+  }
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`flex items-center justify-center gap-1 px-6 py-3 mt-auto capitalize rounded-lg ${
+        full ? "w-full" : ""
+      } ${bgClassName} ${className}`}
+    >
+      {children}
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+    </button>
+  );
+};
+
+export default Button;
