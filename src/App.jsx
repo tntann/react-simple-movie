f8102a1e8777ea1f237e525092197f57a1d283be
tntan <nhattan8421@gmail.com>
2024-06-07 21:58:06 +0700

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
-import { Fragment } from "react";
+import { Fragment, lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import "swiper/css";
 import Main from "./components/layout/Main";
-import HomePage from "./pages/HomePage";
-import MoviePage from "./pages/MoviePage";
-import TvSeriesPage from "./pages/TvSeriesPage";
-import AnimePage from "./pages/AnimePage";
-import MoviesDetailsPage from "./pages/MoviesDetailsPage";
+// import HomePage from "./pages/HomePage";
+// import MoviePage from "./pages/MoviePage";
+// import TvSeriesPage from "./pages/TvSeriesPage";
+// import AnimePage from "./pages/AnimePage";
+// import MoviesDetailsPage from "./pages/MoviesDetailsPage";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const MoviePage = lazy(() => import("./pages/MoviePage"));
+const MoviesDetailsPage = lazy(() => import("./pages/MoviesDetailsPage"));
+const TvSeriesPage = lazy(() => import("./pages/TvSeriesPage"));
+const AnimePage = lazy(() => import("./pages/AnimePage"));
 
 function App() {
   return (
     <Fragment>
-      <Routes>
-        <Route element={<Main></Main>}>
-          <Route path="/" element={<HomePage></HomePage>}></Route>
-          <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
-          <Route
-            path="/movie/:movieId"
-            element={<MoviesDetailsPage></MoviesDetailsPage>}
-          ></Route>
-          <Route
-            path="tvseries"
-            element={<TvSeriesPage></TvSeriesPage>}
-          ></Route>
-          <Route path="anime" element={<AnimePage></AnimePage>}></Route>
-        </Route>
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route element={<Main></Main>}>
+            <Route path="/" element={<HomePage></HomePage>}></Route>
+            <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+            <Route
+              path="/movie/:movieId"
+              element={<MoviesDetailsPage></MoviesDetailsPage>}
+            ></Route>
+            <Route
+              path="tvseries"
+              element={<TvSeriesPage></TvSeriesPage>}
+            ></Route>
+            <Route path="anime" element={<AnimePage></AnimePage>}></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </Fragment>
   );
 }
