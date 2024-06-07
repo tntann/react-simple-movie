fa174d471a27346e3d9cdfca70a5d9683d03d531
tntan <nhattan8421@gmail.com>
2024-06-07 21:57:35 +0700

    update react-simple-movie

--- src/config.jsx
+++ src/config.jsx
@@ -1,3 +1,16 @@
+//https://api.themoviedb.org/3/movie/now_playing?&api_key=247c6a0e12abe3d34fcf5e59c2032141
 export const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export const apiKey = "247c6a0e12abe3d34fcf5e59c2032141";
-//https://api.themoviedb.org/3/movie/now_playing?&api_key=247c6a0e12abe3d34fcf5e59c2032141
+const tmdbEndpoint = "https://api.themoviedb.org/3/movie";
+const tmdbEndpointSearch = "https://api.themoviedb.org/3/search/movie";
+export const tmdbAPI = {
+  getMovieList: (type, page = 1) =>
+    `${tmdbEndpoint}/${type}?&api_key=${apiKey}&page=${page}`,
+  getMovieDetails: (movieId) => `${tmdbEndpoint}/${movieId}?&api_key=${apiKey}`,
+  getMovieInfo: (movieId, type) =>
+    `${tmdbEndpoint}/${movieId}/${type}?&api_key=${apiKey}`,
+  getMovieSearch: (query, page) =>
+    `${tmdbEndpointSearch}?&api_key=${apiKey}&query=${query}&page=${page}`,
+  imageOriginal: (url) => `https://image.tmdb.org/t/p/original/${url})`,
+  image500: (url) => `https://image.tmdb.org/t/p/w500/${url}`,
+};
